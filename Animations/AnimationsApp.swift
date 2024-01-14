1830aa6bf6206ec78328dfbd7382f851db76ffd5
wtrieu <waylantrieu@gmail.com>
2024-01-13 20:20:20 -0800

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Waylan Trieu on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
